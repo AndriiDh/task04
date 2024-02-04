d7489d402c733069c008c05565ca1d935da67bec
Andrii Dudash <93321076+AndriiDh@users.noreply.github.com>
2024-02-04 14:09:49 +0200

    Add lambda handlers

---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,25 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.ResourceType;
+
+@LambdaHandler(lambdaName = "sqs_handler",
+        roleName = "sqs_handler-role",
+        runtime = DeploymentRuntime.JAVA8
+)
+@DependsOn(name = "async_queue", resourceType = ResourceType.SQS_QUEUE)
+@SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 10)
+public class SqsHandler implements RequestHandler<Object, String> {
+
+    public String handleRequest(Object request, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("SQS event: " + request);
+        return "SUCCESS";
+    }
+}
